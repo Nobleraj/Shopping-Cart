ccde24a8917fa9f6f9768ee7ad197416ecb84f6d
Nobleraj <nobleraj.raj@gmail.com>
2022-08-25 11:48:51 +0530

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,13 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function App() {
   let navigate = useNavigate();
+  let random = Math.round(Math.random() * 5);
 
   useEffect(() => {
-    //navigate('/product');
+    console.log('val', random);
+    if (random >= 1) {
+      //navigate('/product');
+    }
   }, []);
 
   return (
